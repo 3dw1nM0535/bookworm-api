6e6ed89902da3c308961f6448006df2807df6cec
3dw1nM0535 <workockmoses@gmail.com>
2018-01-21 20:39:50 +0300

    user signup and server-side validation

--- src/index.js
+++ src/index.js
@@ -4,19 +4,21 @@ import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 
+import auth from './routes/auth';
+import users from './routes/users';
+
 // init private keys
 dotenv.config();
-
-import auth from './routes/auth';
 
 const app = express();
 mongoose.connect(process.env.MONGODB_URI);
 
-//parse application/json data
+// parse application/json data
 app.use(bodyParser.json());
 
 // route mounting middleware config
 app.use('/api/auth', auth);
+app.use('/api/users', users);
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
--- src/models/users.js
+++ src/models/users.js
@@ -1,17 +1,23 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import uniqueValidator from 'mongoose-unique-validator';
 
 const schema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     lowercase: true,
-    index: true
+    index: true,
+    unique: true
   },
   passwordHash: {
     type: String,
     required: true
+  },
+  confirmed: {
+    type: Boolean,
+    default: false
   }
 }, {timestamps: true});
 
@@ -22,6 +28,11 @@ const schema = new mongoose.Schema({
 // password comparison
 schema.methods.isValidPassword = function isValidPassword (password) {
   return bcrypt.compareSync(password, this.passwordHash);
+};
+
+// set password
+schema.methods.setPassword = function setPassword (password) {
+  this.passwordHash = bcrypt.hashSync(password, 10);
 };
 
 // generate web token for user credentials encryption
@@ -35,9 +46,13 @@ schema.methods.generateJWT = function generateJWT () {
 schema.methods.toAuthJSON = function toAuthJSON () {
   return {
     email: this.email,
+    confirmed: this.confirmed,
     token: this.generateJWT()
   }
 }
+
+// uniqueness validation plugin
+schema.plugin(uniqueValidator, { message: 'This email is already taken' });
 
 
 export default mongoose.model('User', schema);
